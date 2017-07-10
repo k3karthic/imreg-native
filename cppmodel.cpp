2c9d98c18f4a24a81251e228d610a76bc9c7632a
Karthic Kumaran Krisnakumar <k3.karthic@live.com>
2017-07-11 02:00:02 +0530

    Change to resnet-152 model

--- cppmodel.cpp
+++ cppmodel.cpp
@@ -5,31 +5,34 @@
 // Implementation
 //
 
-CppModel::CppModel(MXNetPredict p): predictor(p), m_thumbnailSrc("qrc:/images/connected.png"), m_labels(QStringList()), loading(true) {}
+CppModel::CppModel(MXNetPredict p)
+    : predictor(p), m_thumbnailSrc("qrc:/images/connected.png"),
+      m_labels(QStringList()), loading(true) {}
 
 // Properties
 
 void CppModel::setThumbnailSrc(const QString &a) {
-    if (a != m_thumbnailSrc) {
-        QString aCopy(a);
-        aCopy.remove(0, 8);
+  if (a != m_thumbnailSrc) {
+    QString aCopy(a);
+    aCopy.remove(0, 8);
 
-        this->m_thumbnailSrc = a;
-        emit thumbnailSrcChanged();
+    this->m_thumbnailSrc = a;
+    emit thumbnailSrcChanged();
 
-        MyImage im;
-        auto im_data = im.processImage(aCopy);
-        auto labels = this->predictor.getPredictions(im_data, 5);
+    MyImage im;
+    auto im_data = im.processImage(aCopy);
+    auto labels = this->predictor.getPredictions(im_data, 5);
 
-        this->m_labels.clear();
+    this->m_labels.clear();
 
-        for (int i = 0; i < static_cast<int> (labels.size()); i++ ){
-            auto x = labels[i];
-            float p = x.first;
-            QString l = x.second;
-            this->m_labels.append(QString("(%1) - %2").arg(QString::number(p, 'g', 2)).arg(l));
-        }
-
-        emit labelsChanged();
+    for (int i = 0; i < static_cast<int>(labels.size()); i++) {
+      auto x = labels[i];
+      float p = x.first;
+      QString l = x.second;
+      this->m_labels.append(
+          QString("(%1) - %2").arg(QString::number(p, 'g', 2)).arg(l));
     }
+
+    emit labelsChanged();
+  }
 }
--- main.cpp
+++ main.cpp
@@ -5,18 +5,17 @@
 #include "cppmodel.hpp"
 #include <mxnetpredict.hpp>
 
-int main(int argc, char *argv[])
-{
-    QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
+int main(int argc, char *argv[]) {
+  QCoreApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+  QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QGuiApplication app(argc, argv);
 
-    MXNetPredict p("model", DevType::cpu, 0);
-    CppModel model(p);
+  MXNetPredict p("model", DevType::cpu, 0);
+  CppModel model(p);
 
-    QQmlApplicationEngine engine;
-    engine.rootContext()->setContextProperty("CppModel", &model);
-    engine.load(QUrl(QLatin1String("qrc:/main.qml")));
+  QQmlApplicationEngine engine;
+  engine.rootContext()->setContextProperty("CppModel", &model);
+  engine.load(QUrl(QLatin1String("qrc:/main.qml")));
 
-    return app.exec();
+  return app.exec();
 }
